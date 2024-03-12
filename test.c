6153ec0d37400da4ac64b350e1fc06b2bf8aa1c8
Chutobu <tombouchut18@gmail.com>
2024-03-12 10:44:57 +0100

    ajout test

---
+++ test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool chainage_arriere(char But,char *base_regles,char *base_faits){
+    bool resultat = FALSE;
+    FILE* regle=fopen(base_regles,"r");
+    if (base_regles != NULL){
+        puts("CHEH");
+        fclose(base_regles);
+    } 
+    do {
+        fgetc
+    }
+}
+
+
+
+int main(){
+
+}
