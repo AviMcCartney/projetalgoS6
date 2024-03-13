0ace56ff0e7639d72bb917837abd413156b8668b
Chutobu <tombouchut18@gmail.com>
2024-03-13 23:53:29 +0100

    error stdio handle abortion

---
+++ memoire.h
@@ -0,0 +1,18 @@
+/**
+ * @file memoire.h
+ * @author Alexandre, Tom, Yanis, Charlotte
+ * @brief
+ * @version 0.1
+ * @date 2024-03-12
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+#ifndef MEMOIRE_H
+#define MEMOIRE_H
+#include <stdio.h>
+#include <stdint.h>
+
+void *allouer_malloc(int taille);
+
+#endif // MEMOIRE_H
